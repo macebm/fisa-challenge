c11c1f0f1b7c2d213c778479a2bfc1b387e36f6b
Mace <marcelbauerat@gmail.com>
2021-01-27 11:32:27 +0100

    Add Text to Card

--- src/components/Extra.js
+++ src/components/Extra.js
@@ -1,10 +1,13 @@
 import QuizCard from "./QuizCard";
 
 export default function Extra() {
-
   return (
     <section className="extra">
-        <QuizCard question="Foo?" answer="Bar!" />
+      <QuizCard question="Foo?" answer="Bar!" />
+      <QuizCard
+        question="Klick on this button"
+        answer="Danke nochmal für die Herausforderung. Es hat wircklich Spaß gemacht und ich hoffe bald mehr bei euch lernen zu dürfen. LG Marcel"
+      />
     </section>
   );
 }
